f818dcc687cb569b761eb337df740d917ee49581
vanessasousapro <vanessasousa.pro@gmail.com>
2025-10-24 13:38:47 -0300

    chore: init project with Oracle DB config and test controller

---
+++ src/main/java/br/com/finap/fintech_finap_backend/FintechFinapBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.finap.fintech_finap_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechFinapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintechFinapBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/finap/fintech_finap_backend/FintechFinapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.finap.fintech_finap_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechFinapBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
