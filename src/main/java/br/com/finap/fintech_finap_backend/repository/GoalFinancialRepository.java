740a720ba66aa2678737793c388a41bd390b191c
vanessasousapro <vanessasousa.pro@gmail.com>
2025-10-31 15:39:35 -0300

    feature: create GoalFinancialRepository interface for data access

---
+++ src/main/java/br/com/finap/fintech_finap_backend/repository/GoalFinancialRepository.java
@@ -0,0 +1,7 @@
+package br.com.finap.fintech_finap_backend.repository;
+
+import br.com.finap.fintech_finap_backend.model.GoalFinancial;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GoalFinancialRepository extends CrudRepository<GoalFinancial, Long> {
+}
